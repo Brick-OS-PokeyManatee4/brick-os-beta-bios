f174bbeb56d0aca060dcd3fc2ff771d60a2517bc
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-22 15:01:41 -0600

    There

--- bootloader.asm
+++ bootloader.asm
@@ -1,55 +1,16 @@
 
 
-mov ah, 0x0e
 
-mov al, 'B'
-int 0x10
-
-mov al, 'r'
-int 0x10
-
-mov al, 'i'
-int 0x10
-
-mov al, 'c'
-int 0x10
-
-mov al, 'k'
-int 0x10
-
-mov al, ' '
-int 0x10
-
-mov al, 'O'
-int 0x10
-
-mov al, 'S'
-int 0x10
-
-mov al, ' '
-int 0x10
-
-mov al, 'L'
-int 0x10
-
-mov al, 'o'
-int 0x10
-
-mov al, 'a'
-int 0x10
-
-mov al, 'd'
-int 0x10
-
-mov al, 'e'
-int 0x10
-
-mov al, 'd'
-int 0x10
+call PrintString
 
 jmp $
 
-
+PrintString:
+  ret
+  
+TestString:
+  db 'Brick OS Loaded',0
 times 510-($-$$) db 0
-
+  
+  
 dw 0xaa55
