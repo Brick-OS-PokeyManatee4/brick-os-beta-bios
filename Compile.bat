1c36f1b31c3a80ea43447f0076ffc420e3fcad98
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-23 10:28:17 -0600

    Update Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -2,4 +2,6 @@ mkdir bin
 nasm bootloader.asm -f bin -o bootloader.bin
 nasm ExtendedProgram.asm -f bin -o ExtendedProgram.bin
 
-copy /b bootloader.bin+ExtendedProgram.bin bin/bootloader.flp
+copy /b bootloader.bin+ExtendedProgram.bin bootloader.flp
+
+move bootloader.flp bin/bootloader.flp
