cdf69708e1a8c73825a87a0a7979d32bbf4f26fc
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-22 18:38:10 -0600

    Fix Again

--- bootloader.asm
+++ bootloader.asm
@@ -4,14 +4,14 @@
 mov bp, 0x7c00
 mov sp, bp
 
-mov bx, TestString
+mov bx TestString
 call PrintString
 
 jmp $
 
 PrintString:
-  mov ah 0x0e
-  mov al [bx]
+  mov ah, 0x0e
+  mov al, [bx]
   int 0x10
   ret
   
@@ -19,5 +19,5 @@ TestString:
   db 'Brick OS Loaded',0
   
 times 510-($-$$) db 0
-  
+
 dw 0xaa55
