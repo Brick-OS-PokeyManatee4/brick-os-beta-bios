158f4a6b8ff44c0f66001eb716da6fc6d10c125d
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-22 19:00:13 -0600

    Print With Less Code

--- bootloader.asm
+++ bootloader.asm
@@ -11,8 +11,13 @@ jmp $
 
 PrintString:
   mov ah, 0x0e
-  mov al, [bx]
-  int 0x10
+  .Loop:
+  cmp [bx], byte 0
+  je .Exit
+    mov al, [bx]
+    int 0x10
+    jmp .Loop
+  .Exit:
   ret
   
 TestString:
