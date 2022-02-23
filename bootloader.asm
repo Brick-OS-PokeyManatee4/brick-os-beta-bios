e6dce1ef871ad45070e7edac03455f0b5db74185
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-22 19:43:50 -0600

    Clean Code

--- bootloader.asm
+++ bootloader.asm
@@ -9,21 +9,8 @@ call PrintString
 
 jmp $
 
-PrintString:
-  mov ah, 0x0e
-  .Loop:
-  cmp [bx], byte 0
-  je .Exit
-    mov al, [bx]
-    int 0x10
-    inc bx
-    jmp .Loop
-  .Exit:
-  ret
-  
-TestString:
-  db 'Brick OS Loaded',0
-  
+%include "Print.asm"
+
 times 510-($-$$) db 0
 
 dw 0xaa55
