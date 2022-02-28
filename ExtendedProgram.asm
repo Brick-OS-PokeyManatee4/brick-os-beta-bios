0dad1ab600a9ffe45dcfe86bb8639fb2fd10cad9
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-28 14:55:33 -0600

    Ok

--- ExtendedProgram.asm
+++ ExtendedProgram.asm
@@ -1,3 +1,13 @@
-db "!"
+[org 0x7e00]
+
+mov bx, ExtendedSpaceSuccess
+call PrintString
+
+jmp $
+
+%include "Print.asm"
+
+ExtendedSpaceSuccess:
+  db 'We Are Succesfully In Extended Space!', 0
 
 times 2048-($-$$) db 0
