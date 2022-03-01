edac0373c2d038e84beb16ae9717d1eb1bd89900
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-01 17:27:51 -0600

    ;=;

--- ExtendedProgram.asm
+++ ExtendedProgram.asm
@@ -2,11 +2,17 @@
 
 jmp EnterProtectedMode
 
+%include "gdt.asm"
 %include "Print.asm"
 
 EnterProtectedMode:
+    call EnableA20
     cli
-    jmp $
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp Codeseg:StartProtectedMode
 
 EnableA20:
   in al, 0x92
@@ -14,6 +20,19 @@ EnableA20:
   out 0x92, al
   ret
 
+[bits 32]
 
+StartProtectedMode:
+
+        mov ax, dataseg
+        mov ds, ax
+	    mov ss, ax
+	    mov es, ax
+	    mov fs, ax
+        mov gs, ax
+        
+        mov [0xb8000], ';'
+
+        jmp $
 
 times 2048-($-$$) db 0
