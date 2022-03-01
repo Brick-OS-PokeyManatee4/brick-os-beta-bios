02c113c1a2cdad3fe5506163b93a51726a85fa09
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-01 16:52:19 -0600

    Here

--- ExtendedProgram.asm
+++ ExtendedProgram.asm
@@ -1,13 +1,19 @@
 [org 0x7e00]
 
-mov bx, ExtendedSpaceSuccess
-call PrintString
-
-jmp $
+jmp EnterProtectedMode
 
 %include "Print.asm"
 
-ExtendedSpaceSuccess:
-  db 'We Are Succesfully In Extended Space!', 0
+EnterProtectedMode:
+    cli
+    jmp $
+
+EnableA20:
+  in al, 0x92
+  or al, 2
+  out 0x92 al
+  ret
+
+
 
 times 2048-($-$$) db 0
