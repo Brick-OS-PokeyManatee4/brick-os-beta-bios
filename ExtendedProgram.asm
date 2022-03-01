e55816c9946e7fa891d0e64cc62327df816f744a
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-01 17:36:16 -0600

    Print

--- ExtendedProgram.asm
+++ ExtendedProgram.asm
@@ -31,7 +31,21 @@ StartProtectedMode:
 	mov fs, ax
         mov gs, ax
         
-        mov [0xb8000], byte ';'
+        mov [0xb8000], byte 'B'
+	mov [0xb8002], byte 'r'
+	mov [0xb8004], byte 'i'
+	mov [0xb8006], byte 'c'
+	mov [0xb8008], byte 'k'
+	mov [0xb80010], byte ' '
+	mov [0xb80012], byte 'O'
+	mov [0xb80014], byte 'S'
+	mov [0xb80016], byte ' '
+	mov [0xb80018], byte 'L'
+	mov [0xb80020], byte 'o'
+	mov [0xb80022], byte 'a'
+	mov [0xb80024], byte 'd'
+	mov [0xb80026], byte 'e'
+	mov [0xb80028], byte 'd'
 
         jmp $
 
