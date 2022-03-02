627e39027ad0ead850fd34caa5f0c3b87a48523e
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-01 18:08:56 -0600

    Fix

--- CPUID.asm
+++ CPUID.asm
@@ -1,34 +1,36 @@
 
 DetectCPUID:
-    pushfd
-    popeax
-    
-    mov ecx, eax
-    
-    xor eax, 1 << 21
-    
-    push eax
-    popfd
-    
-    pushfd
-    pop eax
-    
-    push ecx
-    popfd
-    
-    xor eax,ecx
-    jz NoCPUID
-    ret
+
+	pushfd 
+	pop eax
+
+	mov ecx, eax
+
+	xor eax, 1 << 21
+
+	push eax
+	popfd
+
+	pushfd 
+	pop eax
+
+	push ecx 
+	popfd
+
+	xor eax,ecx
+	jz NoCPUID
+	ret
 
 DetectLongMode:
-    mov eax, 0x800000001
-    cpuid
-    test edx, 1 << 29
-    jz NoLongMode
-    ret
+	mov eax, 0x80000001
+	cpuid
+	test edx, 1 << 29
+	jz NoLongMode
+	ret
 
 NoLongMode:
-    hlt ; No Long Mode Support
-    
+	hlt ; No Long Mode Support
+
 NoCPUID:
-        hlt ; No CPUID Support
+
+	hlt ; No CPUID support.
