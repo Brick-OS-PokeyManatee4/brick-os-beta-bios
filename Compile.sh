7cf2a5aa16504c826235c169ea18f74865ae45a3
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-08 10:36:09 -0600

    There

--- Compile.sh
+++ Compile.sh
@@ -1,8 +1,13 @@
 #/bin/bash
+echo Making Dirs
 mkdir bin
+echo Done Making Dirs
+echo Building
 
 nasm bootloader.asm -f bin -o bin/bootloader.bin
 nasm ExtendedProgram.asm -f bin -o bin/ExtendedProgram.bin
+echo Done Building
+echo Merging
 cat bin/bootloader.bin bin/ExtendedProgram.bin > bin/bootloader.flp
-
-echo Done
+echo Done Merging
+echo All Done
