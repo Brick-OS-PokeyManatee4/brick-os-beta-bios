6c16784fc794e99ba73a125ed382c7901777f188
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-09 02:40:15 -0600

    MILK

--- ExtendedProgram.asm
+++ ExtendedProgram.asm
@@ -54,13 +54,15 @@ StartProtectedMode:
 	call EditGDT
         jmp codeseg:Start64Bit
 	
-[bits 64]	
+[bits 64]
+[extern _start]
 	
 Start64Bit:
 	mov edi, 0xb8000
 	mov rax, 0x1f201f201f201f20
 	mov ecx, 500
 	rep stosq
+	call _start
 	jmp $
 
 times 2048-($-$$) db 0
