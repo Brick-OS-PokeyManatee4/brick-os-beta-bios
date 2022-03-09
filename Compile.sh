c4f3e9925af6d85ddc9cf670f9c7bed563b533f3
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-09 12:07:29 -0600

    Fix

--- Compile.sh
+++ Compile.sh
@@ -7,15 +7,15 @@ echo Building
 
 nasm bootloader.asm -f bin -o bin/bootloader.bin
 nasm ExtendedProgram.asm -f elf64 -o bin/ExtendedProgram.o
-gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c "Kernel.cpp" -o "Kernel.o"
+gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c "Kernel.cpp" -o "bin/Kernel.o"
 echo Done Building
 echo Linking
-ld -o "Kernel.tmp" -Ttext 0x7e00 bin/ExtendedProgram.o Kernel.o
+ld -o "Kernel.tmp" -Ttext 0x7e00 bin/ExtendedProgram.o bin/Kernel.o
 echo Done Linking
 echo Objcoping
-objcopy -O binary Kernel.tmp bin/Kernel.bin
+objcopy -O binary Kernel.tmp bin/kernel.bin
 echo Done Objcoping
 echo Merging
-cat bin/bootloader.bin bin/Kernel.bin > bin/bootloader.flp
+cat bin/bootloader.bin bin/kernel.bin > bin/bootloader.flp
 echo Done Merging
 echo All Done
