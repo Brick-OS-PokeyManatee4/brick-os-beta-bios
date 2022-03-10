8ecf68100e6cfd679a53813fd476954b2c26afce
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-10 13:56:11 -0600

    Fix

--- DiskRead.asm
+++ DiskRead.asm
@@ -1,30 +1,30 @@
 
-PROGRAM_SPACE equ 0x7e00
+PROGRAM_SPACE equ 0x8000
 
 ReadDisk:
 
-  mov ah, 0x02
-  mov bx, PROGRAM_SPACE
-  mov al, 4
-  mov dl, [BOOT_DISK]
-  mov ch, 0x00
-  mov dh, 0x00
-  mov cl, 0x02
-  
-  int 0x13
-  
-  jc DiskReadFailed
-  
-  ret
+	mov ah, 0x02
+	mov bx, PROGRAM_SPACE
+	mov al, 32
+	mov dl, [BOOT_DISK]
+	mov ch, 0x00
+	mov dh, 0x00
+	mov cl, 0x02
+
+	int 0x13
+
+	jc DiskReadFailed
+
+	ret
 
 BOOT_DISK:
-  db 0
-  
+	db 0
+
 DiskReadErrorString:
-  db 'Disk Reading Fail:( ;-;',0
-  
+	db 'Disk Read Failed',0
+
 DiskReadFailed:
-  mov bx, DiskReadErrorString
-  call PrintString
-  
-  jmp $
+	mov bx, DiskReadErrorString
+	call PrintString
+
+	jmp $
