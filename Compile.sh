0946a68e5d7116d9701a663fede1f94c5f8fa75d
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-16 16:30:44 -0500

    Fix

--- Compile.sh
+++ Compile.sh
@@ -5,13 +5,22 @@ mkdir bin
 echo Done Making Dirs
 echo Building
 nasm bootloader.asm -f bin -o bootloader.bin
-nasm ExtendedProgram.asm -f bin -o ExtendedProgram.bin
+nasm ExtendedProgram.asm -f elf64 -o ExtendedProgram.o
+gcc -ffrestanding -mno-red-zone -m64 -c "Kernel.cpp" -o "Kernel.o"
 echo Done Building
+echo Linking
+ld -o kernel.tmp -Ttext 0x7e00 ExtendedProgram.o Kernel.o
+echo Done Linking
+echo Objcopying
+objcopy -O binary kernel.tmp kernel.bin
+echo Done Objcopying
 echo Merging
-cat bootloader.bin ExtendedProgram.bin > bin/bootloader.flp
+cat bootloader.bin kernel.bin > bin/bootloader.flp
 echo Done Merging
 echo Cleaning
-rm ExtendedProgram.bin
+rm ExtendedProgram.o
 rm bootloader.bin
+rm Kernel.o
+rm kernel.bin
 echo Done Cleaning
 echo All Done
