967cf2c8fb145a77070871c264c0391bc8873ce2
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-03-28 04:31:45 -0500

    Update Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -1,7 +1,15 @@
-mkdir bin
-nasm bootloader.asm -f bin -o bootloader.bin
-nasm ExtendedProgram.asm -f bin -o ExtendedProgram.bin
+nasm -f bin -o bootloader.bin
+nasm ExtendedProgram.asm -f elf64 -o ExtendedProgram.o
 
-copy /b bootloader.bin+ExtendedProgram.bin bootloader.flp
+gcc -ffreestanding -mno-red-zone -m64 -c "Kernel.cpp" -o "Kernel.o"
+echo Done Building
+echo Linking
+ld -o kernel.tmp -Ttext 0x7e00 ExtendedProgram.o Kernel.o
+echo Done Linking
+echo Objcopying
+objcopy -O binary kernel.tmp Kernel.bin
+echo Done Objcopying
+
+copy /b bootloader.bin+Kernel.bin bootloader.flp
 
 move bootloader.flp bin/bootloader.flp
