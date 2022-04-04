813f06bca2cc8f9030a48f840b8f4ea847b7e9ef
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-04-04 11:50:06 -0500

    Update Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -1,4 +1,7 @@
 mkdir bin
+mkdir bin/boot
+mkdir bin/boot/os
+mkdir bin/isoOutput
 nasm bootloader.asm -f bin -o bootloader.bin
 nasm ExtendedProgram.asm -f elf64 -o ExtendedProgram.o
 gcc -ffreestanding -mno-red-zone -m64 -c "Kernel.cpp" -o "Kernel.o"
@@ -10,6 +13,7 @@ echo Objcopying
 objcopy -O binary kernel.tmp Kernel.bin
 echo Done Objcopying
 
-copy /b bootloader.bin+Kernel.bin bootloader.flp
+copy /b bootloader.bin+Kernel.bin BrickOS.bin
 
-move bootloader.flp bin/bootloader.flp
+move BrickOS.bin bin/boot/os/BrickOS.bin
+copy BrickOS.bin bin/boot/os/BrickOSDebug.bin
