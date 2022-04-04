cc7491396865ac723bbe65954a8ea84115a8fee7
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-04-04 13:20:15 -0500

    Ok

--- ExtendedProgram.asm
+++ ExtendedProgram.asm
@@ -34,6 +34,18 @@ StartProtectedMode:
 	mov fs, ax
 	mov gs, ax
 
+	mov [0xb8000], byte 'H'
+	mov [0xb8002], byte 'e'
+	mov [0xb8004], byte 'l'
+	mov [0xb8006], byte 'l'
+	mov [0xb8008], byte 'o'
+	mov [0xb800a], byte ' '
+	mov [0xb800c], byte 'W'
+	mov [0xb800e], byte 'o'
+	mov [0xb8010], byte 'r'
+	mov [0xb8012], byte 'l'
+	mov [0xb8014], byte 'd'
+
 	call DetectCPUID
 	call DetectLongMode
 	call SetUpIdentityPaging
@@ -50,8 +62,8 @@ Start64Bit:
 	mov ecx, 500
 	rep stosq
 	
-        call _start
-        jmp $
-
-
+	call _start
+	
+	jmp $
+	
 times 2048-($-$$) db 0
