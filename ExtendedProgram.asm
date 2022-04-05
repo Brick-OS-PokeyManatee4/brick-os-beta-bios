15ce7f2008156210825f65838e7723b220cfd801
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-04-05 11:17:16 -0500

    Fix

--- ExtendedProgram.asm
+++ ExtendedProgram.asm
@@ -1,4 +1,22 @@
-section .text
+.set ALIGN,    1<<0
+.set MEMINFO,  1<<1
+.set FLAGS,    ALIGN | MEMINFO
+.set MAGIC,    0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
+
+	mov $stack_top, %esp
 
 jmp EnterProtectedMode
 
